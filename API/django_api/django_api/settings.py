c27c2a70103b6d0413116d947514d1145741e49b
HarshaCR-7 <harshacr007@gmail.com>
2021-10-18 18:55:46 +0530

    commit

--- API/django_api/django_api/settings.py
+++ API/django_api/django_api/settings.py
@@ -103,13 +103,20 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+#    'default': {
+#        'NAME': 'django_api',
+#        'USER': 'postgres',
+ #       'PASSWORD': '123',
+  #      'HOST': 'localhost',
+   #     'PORT': ''
     }
 }
+
 
 
 # Password validation
